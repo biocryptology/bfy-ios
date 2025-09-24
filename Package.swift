6c3e23b469cd577d49f7d7775f02262d7a1fb5d9
B-FY <109145945+biocryptology@users.noreply.github.com>
2025-09-24 08:30:35 +0200

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "bfy",
-            url: "https://github.com/biocryptology/bfy-ios-spm/releases/download/v.0.1.7/Bfy.framework.zip",
+            url: "http://nexus.sb.biocryptology.net/repository/bfy-ios/1.0.2/Bfy.framework.zip,
             checksum: "4f3e6ba0fb153d74a7573cc664b97796944b301a0c8cbfe129680e92c6bd2cf0"
         )
     ]
